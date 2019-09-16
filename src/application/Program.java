2660c9565d1d256e4714ac20a19565efef13f896
yuritakeo <37599776+yuritakeo@users.noreply.github.com>
2019-09-16 18:32:43 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entitis.Department;
 import model.entitis.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         Department department = new Department(1, "Books");
+        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         try {
             Seller seller = new Seller(1, "Yuri", "yuri@gmail.com", new Date(sdf.parse("26/08/1992").getTime()), 1800.00, department);
