3b9edf6d8ab1c70de8e6b07e8596cfca17d92442
yuritakeo <37599776+yuritakeo@users.noreply.github.com>
2019-09-16 17:37:12 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,12 @@ package application;
 import java.sql.Connection;
 import db.DB;
 import java.sql.SQLException;
+import model.entitis.Department;
 
 public class Program {
 
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        try {
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Department department = new Department(1, "Books");
+        System.out.println(department);
     }
 }
---
+++ src/model/entitis/Department.java
@@ -0,0 +1,70 @@
+package model.entitis;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 1;
+
+    private int id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.id;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "id=" + id + ", name=" + name + '}';
+    }
+}
